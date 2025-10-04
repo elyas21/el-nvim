e2cfbf4f1992d864e277c8118c18abba70804951
Elyas Abate <elyasabate21@Gmail.com>
2025-10-04 12:23:38 +0300

    small things mapping

--- lua/custom/keymap.lua
+++ lua/custom/keymap.lua
@@ -1,4 +1,57 @@
+--[[
+vim.g.mapleader = " "
 
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
+vim.keymap.set("n", "<space>x", ":.lua<CR>")
+vim.keymap.set("v", "<space>x", ":lua<CR>")
+vim.keymap.set("i", "jk", "<Esc>", { desc = "Escape to Normal Mode" })
+
+--copy
+vim.keymap.set({ "v", "n" }, "<leader>cl", '"+yy', { desc = "Copy all to clipboard" })
+vim.keymap.set({ "v", "n" }, "<leader>ca", 'ggvG"+y', { desc = "Copy all to clipboard" })
+vim.keymap.set({ "v" }, "<leader>c", '"+y', { desc = "Copy all to clipboard" })
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+local keys_to_disable = { "<Up>", "<Down>", "<Left>", "<Right>" }
+for _, key in ipairs(keys_to_disable) do
+	vim.keymap.set({ "n", "i", "v" }, key, "<Nop>", { noremap = true, silent = true })
+end
+local job_id = nil
+
+vim.keymap.set("n", "<space>st", function()
+	vim.cmd.vnew()
+	vim.cmd.term()
+	vim.cmd.wincmd("J")
+	vim.api.nvim_win_set_height(0, 5)
+	job_id = vim.bo.channel
+	print("Terminal created with job_id:", job_id)
+end)
+
+vim.keymap.set("n", "<space>tt", function()
+	-- Check if terminal channel is valid
+	if not job_id or job_id <= 0 then
+		-- Create new terminal
+		print("Creating new terminal...")
+		vim.cmd.vnew()
+		vim.cmd.term()
+		vim.cmd.wincmd("J")
+		vim.api.nvim_win_set_height(0, 5)
+		job_id = vim.bo.channel
+		print("New terminal created with job_id:", job_id)
+	end
+
+	-- Send command to terminal
+	local success, err = pcall(vim.fn.chansend, job_id, "echo 'hi'\r\n")
+	if not success then
+		print("Failed to send command:", err)
+		-- Reset job_id so it creates a new terminal next time
+		job_id = nil
+	end
+end)
+
+]]
 
 vim.keymap.set('n', '<C-u>', '<C-u>zz', {noremap=true, silent = true})
 vim.keymap.set('n', '<C-d>', '<C-d>zz', {noremap=true, silent = true})
